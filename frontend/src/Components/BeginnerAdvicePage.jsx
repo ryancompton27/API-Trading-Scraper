00e5378467e5bd11297071b4a9d97de264a3e749
Rebekah-99 <rebekah.driver@yahoo.ca>
2026-03-29 12:14:26 -0400

    Work on beginner-advice-page

--- frontend/src/Components/BeginnerAdvicePage.jsx
+++ frontend/src/Components/BeginnerAdvicePage.jsx
@@ -1,8 +1,36 @@
 
+
 export default function BeginnerAdviceApp() {
     return (
         <div>
-            <h1>Beginner Articles Page</h1>
+            <div class="beginner-advice-title">
+                <h1>Beginner Articles Page</h1>
+            </div>
+            <div class="beginner-advice-section">
+                <div class="begginner-advice-links-text">
+                <h3>Below are some articles to get you started with understanding the world of investing.</h3>
+                <h3>We try to provide a variety of resources to give you a variety of ways to understand!</h3>
+                </div>
+                    <div class="beginner-advice-links">
+                    <p>This article is a great starting place if you've not read up on investing.</p>
+                    <a href="https://www.wealthsimple.com/en-ca/learn/investing-basics#beginners_investing_tips" target="_blank">Investing Basics</a>
+                    <br/>
+                    <p>This article is a great place to start if you think you're ready to begin investing</p>
+                    <a href="https://www.getsmarteraboutmoney.ca/learning-path/getting-started/getting-started-with-investing/" target="_blank">Things to consider before you invest</a>
+                    <br/>
+                    <p>This article should provide some adivce on setting goals when it comes to defining your reasons for investing.</p>
+                    <a href="https://www.cooperators.ca/en/personal/resource-centre/plan-ahead/investing-for-beginners" target="_blank">Setting your goals before you invest</a>
+                    <br/>
+                    <p>Some advice from your Country on investing</p>
+                    <a href="https://www.canada.ca/en/financial-consumer-agency/services/savings-investments/investing-basics.html"target="_blank">Investing Informaion from the Government of Canada</a>
+                    <br/>
+                    </div>
+                <div class="begginner-advice-links-text">
+                <h4>A reminder that the articles above are there to help you learn, and do not take the place of professional adivce.</h4>
+                <h4>Our goal here is simple - to have you leave this site with more knowledge then before on what investing is and</h4>
+                <h4>why you might want to begin investing. We hope these articles help you in that!</h4>
+                </div>
+            </div>
         </div>
     )
 }
