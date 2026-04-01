ab65478153b8b7ed6f427ce23183285d9a688c87
Richardcompton27 <richardcompton2005@gmail.com>
2026-04-01 12:10:55 -0400

    added basic functionality to website home page

--- frontend/src/Components/InvestAdviceApp.jsx
+++ frontend/src/Components/InvestAdviceApp.jsx
@@ -1,8 +1,32 @@
+import BeginnerAdviceApp from './BeginnerAdvicePage'
+import InvestAdviceStocks from './InvestAdviceStocks'
+//import { useNavigate } from "react-router-dom"
 
 export default function InvestAdviceApp() {
+ //   const navigate = useNavigate();
+    
     return (
+        
         <div>
-            <h1>Home Page</h1>
+            <div class="topnav">
+                <button /*onClick={() => navigate(<InvestAdviceApp />)}*/ class="active">Home</button>
+                <button /*onClick={navigate(<BeginnerAdviceApp />)}*/>Advice</button>
+                <button onClick={<InvestAdviceStocks />}>Stocks</button>
+               
+            </div>
+            <h1>Welcome to our platform</h1>
+
+            <div>
+                <h2>about us</h2>
+
+                <h3>our goal is to deliver a easy to use and well made trading platform that allows
+                    new users to trade with ease and peace of mind.
+                </h3>
+
+            </div>
+
         </div>
     )
+
+
 }
