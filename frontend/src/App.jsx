95a506f59edb01886ab5fe1904fcfa72c99b5a05
Richardcompton27 <richardcompton2005@gmail.com>
2026-04-10 10:41:25 -0400

    fixed navigation and got it to properly navigate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
 import './App.css'
-import InvestAdviceApp from './Components/InvestAdviceApp'
+import { Routes, Route } from "react-router-dom"
+import InvestAdviceApp from "./Components/InvestAdviceApp"
+import BeginnerAdviceApp from "./Components/BeginnerAdvicePage"
+import InvestAdviceStocks from "./Components/InvestAdviceStocks"
 
 function App() {
 
   return (
     <>
       <div>
-        <InvestAdviceApp />
-
+        
+        <Routes>
+        <Route path="/" element={<InvestAdviceApp />} />
+        <Route path="/advice" element={<BeginnerAdviceApp />} />
+        <Route path="/stocks" element={<InvestAdviceStocks />} />
+      </Routes>
       </div>
     </>
   )
 }
-
 export default App
--- frontend/src/Components/InvestAdviceApp.jsx
+++ frontend/src/Components/InvestAdviceApp.jsx
@@ -1,19 +1,20 @@
-import BeginnerAdviceApp from './BeginnerAdvicePage'
-import InvestAdviceStocks from './InvestAdviceStocks'
-//import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 export default function InvestAdviceApp() {
- //   const navigate = useNavigate();
-    
+    const navigate = useNavigate()
+
     return (
-        
         <div>
-            <div class="topnav">
-                <button /*onClick={() => navigate(<InvestAdviceApp />)}*/ class="active">Home</button>
-                <button /*onClick={navigate(<BeginnerAdviceApp />)}*/>Advice</button>
-                <button onClick={<InvestAdviceStocks />}>Stocks</button>
-               
-            </div>
+        
+        <div className="topnav">
+            <button onClick={() => navigate("/")}>Home</button>
+            <button onClick={() => navigate("/advice")}>Advice</button>
+            <button onClick={() => navigate("/stocks")}>Stocks</button>
+        </div>
+    
+
+    
+
             <h1>Welcome to our platform</h1>
 
             <div>
@@ -26,6 +27,7 @@ export default function InvestAdviceApp() {
             </div>
 
         </div>
+        
     )
 
 
